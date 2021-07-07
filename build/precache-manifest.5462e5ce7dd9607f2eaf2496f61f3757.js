746f05c6eb603681d1efb9a9dcd5f0cd4b85303b
Grace Yang <graceyxt.artdesign@gmail.com>
2021-07-07 09:14:42 -0400

    node_modules

---
+++ build/precache-manifest.5462e5ce7dd9607f2eaf2496f61f3757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb1e5a056b60c2042481b2223408fa7",
+    "url": "/a19-React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "c85635143026af4a75f7",
+    "url": "/a19-React-Employee-Directory/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "50158b2293302883d216",
+    "url": "/a19-React-Employee-Directory/static/css/main.3ae1f97e.chunk.css"
+  },
+  {
+    "revision": "c85635143026af4a75f7",
+    "url": "/a19-React-Employee-Directory/static/js/2.0a6ff45d.chunk.js"
+  },
+  {
+    "revision": "50158b2293302883d216",
+    "url": "/a19-React-Employee-Directory/static/js/main.ceca6dfd.chunk.js"
+  },
+  {
+    "revision": "28ac4c5a2ad5b19e62b0",
+    "url": "/a19-React-Employee-Directory/static/js/runtime~main.72667025.js"
+  }
+]);
